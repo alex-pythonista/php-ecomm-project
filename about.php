48793187eae1a529955efc8bdf2a8aae651b4ba9
jaforsadek <jaforsadek619@gmail.com>
2022-01-03 01:24:35 +0600

    about.php added

---
+++ about.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/about.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+
+        <!--====== NAVBAR TWO PART START ======-->
+
+        <section class="navbar-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <nav class="navbar navbar-expand-lg">
+                           
+                            <a class="navbar-brand" href="#">
+                                <img src="assets/images/logo.svg" alt="Logo">
+                            </a>
+                            
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTwo" aria-controls="navbarTwo" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="toggler-icon"></span>
+                                <span class="toggler-icon"></span>
+                                <span class="toggler-icon"></span>
+                            </button>
+    
+                            <div class="collapse navbar-collapse sub-menu-bar" id="navbarTwo">
+                                <ul class="navbar-nav m-auto">
+                                    <li class="nav-item active"><a class="page-scroll" href="#home">home</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="./login.html">Login</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="./sign up.html">Sign up</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="./about.html">About</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="./team.html">Team</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="./contact.html">Contact</a></li>
+                                </ul>
+                            </div>
+    
+                        </nav> <!-- navbar -->
+                    </div>
+                </div> <!-- row -->
+            </div> <!-- container -->
+        </section>
+    
+        <!--====== NAVBAR TWO PART ENDS ======-->
+<section id="about" class="about-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5">
+                <div class="faq-content mt-45">
+                    <div class="about-title">
+                        <h6 class="sub-title">A Little More About Us</h6>
+                        <h4 class="title">Frequently Asked Questions <br> About Our Site</h4>
+                    </div> <!-- faq title -->
+
+</section>
+
+<!--====== ABOUT PART ENDS ======-->
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
